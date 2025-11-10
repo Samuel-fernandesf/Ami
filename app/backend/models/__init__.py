919d55a3bf93a461abb24ed3f50b0b2fbc89db53
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-11-10 03:41:26 -0300

    Importando novas classes ao pacote.

--- app/backend/models/__init__.py
+++ app/backend/models/__init__.py
@@ -1,5 +1,6 @@
 from .usuario import Usuario
-from .habilidade import Habilidade
+from .habilidade import Habilidade, OportunidadeHabilidade, VoluntarioHabilidade
 from .inscricao import Inscricao
 from .oportunidade import Oportunidade
 from .organizaçao import Organizacao
+from .enums import StatusInscricao, StatusOrganizacao, StatusOportunidades
