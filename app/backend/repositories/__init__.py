5069dc70691a72e0aa7c41b869ddb7f75c2771cf
Bernardo IFSP <bernardo.duarte@aluno.ifsp.edu.br>
2025-11-10 22:23:03 -0300

    esqueci de salvar esse aqui,  -1 linha malz ai fmlKKKKK

--- app/backend/repositories/__init__.py
+++ app/backend/repositories/__init__.py
@@ -1,6 +1,5 @@
 from .habilidades_repo import HabilidadeRepo
 from .user_repo import UserRepo
-from .voluntario_habilidade_repo import VoluntarioHabilidadeRepo
 from .inscricao_repo import InscricaoRepo
 from .oportunidade_repo import OportunidadeRepo
 from .organizacao_repo import OrganizacaoRepo
