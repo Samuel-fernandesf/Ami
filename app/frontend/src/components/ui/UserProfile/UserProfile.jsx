b556b8e1f0f9b98db920f651a2e5d81c13d2c0d5
Yagor Vitor <yagorvitor6@gmail.com>
2025-12-13 14:26:11 -0300

    excluindo o frontend para uma reestruturacao e atualizacao

--- app/frontend/src/components/ui/UserProfile/UserProfile.jsx
+++
@@ -1,8 +0,0 @@
-export default function UserProfile() {
-    return (
-        <section>
-            <h2>User Profile</h2>
-            <p>This is the user profile section.</p>
-        </section>
-    );
-}
